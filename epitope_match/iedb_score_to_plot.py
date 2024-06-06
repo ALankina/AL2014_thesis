a0dda2bc89c2f265a148c93169d7b1a507ba7e06
ALankina <nastyalankina1997@gmail.com>
2024-06-06 22:02:45 +0100

    iedb plot maker added

---
+++ epitope_match/iedb_score_to_plot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+
+def read_results(filename):
+    with open(filename, "r") as f:
+        xs, ys = [], []
+        reader = csv.reader(f)
+        next(reader)
+        for pos, _, score, _ in reader:
+            xs.append(int(pos)), ys.append(float(score))
+        return np.array(xs), np.array(ys)
+
+
+def make_graph(xs, ys, threshold):
+    _, ax = plt.subplots()
+    ax.plot(xs, ys, color="green")
+    # Top
+    ax.fill_between(xs, ys, threshold, where=ys > threshold, color="yellow")
+    # Bottom
+    ax.fill_between(xs, ys, threshold, where=ys < threshold, color="green")
+    # Midline
+    plt.axhline(threshold, color="black")
+    plt.xticks(np.arange(0, max(xs), 50))
+    plt.show()
+
+
+def main():
+    filename = "./epitope_match/gB_bcell_prediction_score.csv"
+    threshold = 0.5
+    xs, ys = read_results(filename)
+    make_graph(xs, ys, threshold)
+
+
+if __name__ == "__main__":
+    main()
